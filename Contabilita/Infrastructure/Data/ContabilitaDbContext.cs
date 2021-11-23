bf5a83d4fada4e42f540bd94986853a28efaa87a
emalo92 <emanuele.longo992@gmail.com>
2021-11-23 20:29:27 +0100

    Added Migration

--- Contabilita/Infrastructure/Data/ContabilitaDbContext.cs
+++ Contabilita/Infrastructure/Data/ContabilitaDbContext.cs
@@ -21,6 +21,15 @@ namespace Infrastructure.Data
         public virtual DbSet<Fattura> Fatture { get; set; } = null!;
         public virtual DbSet<Pagamento> Pagamenti { get; set; } = null!;
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("data source=.; initial catalog = Contabilita; persist security info=True; Integrated Security = SSPI;");
+            }
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Azienda>(entity =>
---
+++ Contabilita/Infrastructure/Data/Migrations/20211123192842_PrimaMigrazione.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class PrimaMigrazione : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aziende",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartitaIVA = table.Column<string>(type: "nchar(11)", fixedLength: true, maxLength: 11, nullable: false),
+                    RagioneSociale = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    Telefono = table.Column<string>(type: "nchar(20)", fixedLength: true, maxLength: 20, nullable: true),
+                    Email = table.Column<string>(type: "nchar(50)", fixedLength: true, maxLength: 50, nullable: true),
+                    Iban = table.Column<string>(type: "nchar(30)", fixedLength: true, maxLength: 30, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aziende", x => x.Id);
+                    table.UniqueConstraint("AK_Aziende_PartitaIVA", x => x.PartitaIVA);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fattura",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numero = table.Column<string>(type: "nchar(20)", fixedLength: true, maxLength: 20, nullable: false),
+                    Data = table.Column<DateTime>(type: "date", nullable: false),
+                    Importo = table.Column<decimal>(type: "decimal(18,0)", nullable: false),
+                    Tipo = table.Column<string>(type: "nchar(20)", fixedLength: true, maxLength: 20, nullable: false),
+                    Azienda = table.Column<string>(type: "nchar(11)", fixedLength: true, maxLength: 11, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fattura", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fattura_Azienda",
+                        column: x => x.Azienda,
+                        principalTable: "Aziende",
+                        principalColumn: "PartitaIVA");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pagamento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modalita = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Data = table.Column<DateTime>(type: "date", nullable: false),
+                    Importo = table.Column<decimal>(type: "decimal(18,0)", nullable: false),
+                    NumAssegnoBonifico = table.Column<string>(type: "nchar(20)", fixedLength: true, maxLength: 20, nullable: true),
+                    Descrizione = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: true),
+                    Azienda = table.Column<string>(type: "nchar(11)", fixedLength: true, maxLength: 11, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagamento", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movimento_Azienda",
+                        column: x => x.Azienda,
+                        principalTable: "Aziende",
+                        principalColumn: "PartitaIVA");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Azienda",
+                table: "Aziende",
+                column: "PartitaIVA",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fattura",
+                table: "Fattura",
+                column: "Numero");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fattura_Azienda",
+                table: "Fattura",
+                column: "Azienda");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagamento_Azienda",
+                table: "Pagamento",
+                column: "Azienda");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fattura");
+
+            migrationBuilder.DropTable(
+                name: "Pagamento");
+
+            migrationBuilder.DropTable(
+                name: "Aziende");
+        }
+    }
+}
