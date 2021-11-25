b76fba4c0a317b03571fcd8c541a7151fb4ba63e
nicola139313 <nicola139313@gmail.com>
2021-11-25 17:10:38 +0100

    azienda form
    fix and add components  azienda form

--- Contabilita/Contabilita/Forms/FormAzienda.Designer.cs
+++ Contabilita/Contabilita/Forms/FormAzienda.Designer.cs
@@ -28,19 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.annulla = new System.Windows.Forms.Button();
+            this.registraFattura = new System.Windows.Forms.Button();
+            this.labeltelefono = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelRegioneSociale = new System.Windows.Forms.Label();
+            this.labelPartitaIva = new System.Windows.Forms.Label();
+            this.titoloAzienda = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.titoloAzienda);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.annulla);
+            this.panel1.Controls.Add(this.registraFattura);
+            this.panel1.Controls.Add(this.labeltelefono);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.labelEmail);
+            this.panel1.Controls.Add(this.labelRegioneSociale);
+            this.panel1.Controls.Add(this.labelPartitaIva);
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 529);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(299, 164);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(203, 23);
+            this.textBox5.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(555, 164);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(203, 23);
+            this.textBox4.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(41, 325);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(210, 23);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(299, 325);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(203, 23);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 164);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 23);
+            this.textBox1.TabIndex = 14;
+            // 
+            // annulla
+            // 
+            this.annulla.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.annulla.Location = new System.Drawing.Point(594, 424);
+            this.annulla.Name = "annulla";
+            this.annulla.Size = new System.Drawing.Size(161, 44);
+            this.annulla.TabIndex = 13;
+            this.annulla.Text = "Annulla";
+            this.annulla.UseVisualStyleBackColor = true;
+            // 
+            // registraFattura
+            // 
+            this.registraFattura.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.registraFattura.Location = new System.Drawing.Point(368, 424);
+            this.registraFattura.Name = "registraFattura";
+            this.registraFattura.Size = new System.Drawing.Size(161, 44);
+            this.registraFattura.TabIndex = 12;
+            this.registraFattura.Text = "Registra";
+            this.registraFattura.UseVisualStyleBackColor = true;
+            // 
+            // labeltelefono
+            // 
+            this.labeltelefono.AutoSize = true;
+            this.labeltelefono.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labeltelefono.Location = new System.Drawing.Point(279, 267);
+            this.labeltelefono.Name = "labeltelefono";
+            this.labeltelefono.Size = new System.Drawing.Size(72, 21);
+            this.labeltelefono.TabIndex = 11;
+            this.labeltelefono.Text = "Telefono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(21, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 21);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Iban";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labelEmail.Location = new System.Drawing.Point(555, 109);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(57, 21);
+            this.labelEmail.TabIndex = 9;
+            this.labelEmail.Text = "E-mail";
+            // 
+            // labelRegioneSociale
+            // 
+            this.labelRegioneSociale.AutoSize = true;
+            this.labelRegioneSociale.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labelRegioneSociale.Location = new System.Drawing.Point(279, 109);
+            this.labelRegioneSociale.Name = "labelRegioneSociale";
+            this.labelRegioneSociale.Size = new System.Drawing.Size(123, 21);
+            this.labelRegioneSociale.TabIndex = 8;
+            this.labelRegioneSociale.Text = "Regione Sociale";
+            // 
+            // labelPartitaIva
+            // 
+            this.labelPartitaIva.AutoSize = true;
+            this.labelPartitaIva.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labelPartitaIva.Location = new System.Drawing.Point(21, 109);
+            this.labelPartitaIva.Name = "labelPartitaIva";
+            this.labelPartitaIva.Size = new System.Drawing.Size(87, 21);
+            this.labelPartitaIva.TabIndex = 7;
+            this.labelPartitaIva.Text = "Partita Iva";
+            // 
+            // titoloAzienda
+            // 
+            this.titoloAzienda.AutoSize = true;
+            this.titoloAzienda.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.titoloAzienda.Location = new System.Drawing.Point(206, 26);
+            this.titoloAzienda.Name = "titoloAzienda";
+            this.titoloAzienda.Size = new System.Drawing.Size(323, 32);
+            this.titoloAzienda.TabIndex = 19;
+            this.titoloAzienda.Text = "Inserimento Nuovo Fornitore\r\n";
             // 
             // FormAzienda
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 557);
+            this.Controls.Add(this.panel1);
             this.Name = "FormAzienda";
             this.Text = "Azienda";
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label labeltelefono;
+        private Label label3;
+        private Label labelEmail;
+        private Label labelRegioneSociale;
+        private Label labelPartitaIva;
+        private Button annulla;
+        private Button registraFattura;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label titoloAzienda;
     }
 }
