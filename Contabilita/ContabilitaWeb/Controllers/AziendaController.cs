8930c65691530d61afa11b212dd2fa56001cc78e
emalo92 <emanuele.longo992@gmail.com>
2021-11-27 17:26:55 +0100

    Added AziendaController + View inserisci

---
+++ Contabilita/ContabilitaWeb/Controllers/AziendaController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using ContabilitaWeb.DTO;
+using Infrastructure.Dal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContabilitaWeb.Controllers
+{
+    public class AziendaController : Controller
+    {
+
+        private readonly ILogger<AziendaController> log;
+        private readonly IContabilitaDal _contabilitaDal;
+        private readonly IMapper _mapper;
+
+        public AziendaController(ILogger<AziendaController> log, IContabilitaDal contabilitaDal, IMapper mapper)
+        {
+            this.log = log;
+            _contabilitaDal = contabilitaDal;
+            _mapper = mapper;
+        }
+
+        public IActionResult Inserisci()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Inserisci(Azienda azienda)
+        {
+            return View();
+        }
+    }
+}
