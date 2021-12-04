d4b0e64c2961fa56cd5aafda8ccb857cd6bc7d1c
emalo92 <emanuele.longo992@gmail.com>
2021-12-04 09:50:30 +0100

    Added FatturaController

---
+++ Contabilita/ContabilitaWeb/Controllers/FatturaController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Infrastructure.Dal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContabilitaWeb.Controllers
+{
+    public class FatturaController : Controller
+    {
+        private readonly ILogger<FatturaController> _logger;
+        private readonly IContabilitaDal _contabilitaDal;
+        private readonly IMapper _mapper;
+
+        public FatturaController(ILogger<FatturaController> log, IContabilitaDal contabilitaDal, IMapper mapper)
+        {
+            _logger = log;
+            _contabilitaDal = contabilitaDal;
+            _mapper = mapper;
+        }
+
+        public IActionResult ViewAll()
+        {
+            return View();
+        }
+    }
+}
