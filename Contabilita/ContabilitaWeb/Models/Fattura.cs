b5a8413788c469fa11064bd818b7808e71a48eee
nicola139313 <nicola139313@gmail.com>
2021-12-10 20:31:50 +0100

    fix models fattura, implements view of fattura

--- Contabilita/ContabilitaWeb/Models/Fattura.cs
+++ Contabilita/ContabilitaWeb/Models/Fattura.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContabilitaWeb.Models
 {
     public class Fattura
     {
         public int Id { get; set; }
+
+        [Display(Name = "Numero")]
         public string Numero { get; set; } = null!;
+
+        [Display(Name ="Data")]
         public DateTime Data { get; set; }
+
+        [Display(Name ="Importo")]
         public decimal Importo { get; set; }
+
+        [Display(Name ="Tipo")]
         public string Tipo { get; set; } = null!;
+
+        [Display(Name ="Azienda")]
         public string Azienda { get; set; } = null!;
     }
 }
