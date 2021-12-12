59d39ed457ddb9ffec6d46a6e78dd89d995062e7
emalo92 <emanuele.longo992@gmail.com>
2021-12-12 11:57:06 +0100

    Fixed tipo fattura + Added PagamentoController

---
+++ Contabilita/ContabilitaWeb/Controllers/PagamentoController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Infrastructure.Dal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContabilitaWeb.Controllers
+{
+    public class PagamentoController : Controller
+    {
+        private readonly ILogger<PagamentoController> _logger;
+        private readonly IContabilitaDal _contabilitaDal;
+        private readonly IMapper _mapper;
+
+        public PagamentoController(ILogger<PagamentoController> log, IContabilitaDal contabilitaDal, IMapper mapper)
+        {
+            _logger = log;
+            _contabilitaDal = contabilitaDal;
+            _mapper = mapper;
+        }
+        public IActionResult ViewAll()
+        {
+            return View();
+        }
+    }
+}
